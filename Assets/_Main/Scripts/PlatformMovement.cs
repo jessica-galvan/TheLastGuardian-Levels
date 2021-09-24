a8b4abd2b2a45ff2d87081ce8d92dddf61bf6061
francorivaas <francoagustinrivas@hotmail.com>
2021-09-23 21:15:33 -0300

    platform with movement

---
+++ Assets/_Main/Scripts/PlatformMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float actualSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualSpeed = speed;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * actualSpeed * Time.deltaTime;
+
+        if (transform.position.y >= 1.8)
+            actualSpeed = -speed;
+        else if (transform.position.y <= -10)
+            actualSpeed = speed;
+
+    }
+}
